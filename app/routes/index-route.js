fb0b28b6ca9dba1b9cea1f03ac7572b617ba99c4
Bruno <brunocesaragweb@gmail.com>
2020-08-20 16:09:19 -0300

    Update index info

--- app/routes/index-route.js
+++ app/routes/index-route.js
@@ -4,14 +4,17 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
+    res.header("Content-Type",'application/json');
+    const indexInfo = JSON.stringify({
         name: "api-service",
         version: "1.0.0",
-        description: "A CRUD API service",
+        description: "A simple api crud service",
         author: "Bruno C. A. Gaudino",
+        personalhomepage: "https://brunogaudino.github.io",
         url: "git+https://github.com/brunogaudino/api-service.git",
         homepage: "https://github.com/brunogaudino/api-service#readme"
-    });
+    },null,2);
+    res.status(200).send(indexInfo);
 });
 
 module.exports = router;
