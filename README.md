38512663a2dda6c42991d6028093989e74246c04
Bruno C. A. Gaudino <brunocesaragweb@gmail.com>
2020-12-16 12:28:52 -0300

    Update gitignore, README and index-route

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# api-service
-A simple api crud service
+# API Service
+
+API created with CRUD (Create, Read, Update and Delete) methods, the API has token authentication methods for validation, mongodb web database.
+
+MVC architecture: controllers folder, models folder, views folder.
+
+Repositories: Database connection files.
+
+Routes: Application routes.
+
+Configs: Global salt key.
+
+Services: Authentication, validation services.
--- app/routes/index-route.js
+++ app/routes/index-route.js
@@ -10,11 +10,11 @@ router.get('/', (req, res, next) => {
     const data = JSON.stringify({
         name: "api-service",
         version: "1.0.0",
-        description: "A simple api service",
         author: "Bruno C. A. Gaudino",
         personalhomepage: "https://brunogaudino.github.io",
         url: "git+https://github.com/brunogaudino/api-service.git",
-        homepage: "https://github.com/brunogaudino/api-service#readme"
+        homepage: "https://github.com/brunogaudino/api-service#readme",
+        description: "API crud service: <br> API created with CRUD (Create, Read, Update and Delete) methods, the API has token authentication methods for validation, mongodb web database.<br> MVC architecture: controllers folder, models folder, views folder.<br> Repositories: Database connection files.<br> Routes: Application routes.<br> Configs: Global salt key.<br> Services: Authentication, validation services."
     },null,2);
     res.status(200).send(data);
 });
